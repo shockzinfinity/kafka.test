fa8ea73b522aa1bae34c2be83d525541d4f14c31
shockz <shockzinfinity@gmail.com>
2022-04-12 18:02:05 +0900

    test projects

---
+++ kafka.client/Program.cs
@@ -0,0 +1,32 @@
+﻿using Confluent.Kafka;
+
+string kafkaEndpoint = "127.0.0.1:9092";
+string kafkaTopic = "testtopic";
+
+var consumerConfig = new ConsumerConfig
+{
+  BootstrapServers = kafkaEndpoint,
+  GroupId = "foo",
+  AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using (var consumer = new ConsumerBuilder<Null, string>(consumerConfig).Build()) {
+  consumer.Subscribe(kafkaTopic);
+
+  var cancelled = false;
+  Console.CancelKeyPress += (_, e) =>
+  {
+    Console.WriteLine("Canceling");
+    e.Cancel = true;
+    cancelled = true;
+  };
+
+  Console.WriteLine("Ctrl-C to exit");
+
+  while (!cancelled) {
+    var consumeResult = consumer.Consume();
+    Console.WriteLine(consumeResult.Message.Value);
+  }
+}
+
+Console.ReadLine();
---
+++ kafka.test/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Confluent.Kafka;
+
+Console.WriteLine("Hello, World!");
+
+string kafkaEndpoint = "127.0.0.1:9092";
+string kafkaTopic = "testtopic";
+
+var confluentConfig = new ProducerConfig
+{
+  BootstrapServers = kafkaEndpoint,
+};
+
+Random random = new Random();
+
+using (var producer = new ProducerBuilder<Null, string>(confluentConfig).Build()) {
+  for (int i = 0; i < 10000; i++) {
+    var message = $"Event {i} : {random.Next(103949)}";
+    //var t = producer.ProduceAsync(kafkaTopic, new Message<Null, string> { Value = message });
+    //t.ContinueWith(task =>
+    //{
+    //  if (task.IsFaulted) {
+    //  } else {
+    //    Console.WriteLine($"Wrote to offset: {task.Result.Offset}");
+    //  }
+    //});
+    Thread.Sleep(50);
+    Console.WriteLine($"published: {message}");
+    await producer.ProduceAsync(kafkaTopic, new Message<Null, string> { Value = message });
+  }
+}
+
+Console.WriteLine("End produce message");
+Console.ReadLine();
